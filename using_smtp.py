f60196a9c6bfb706154a029d6f58b7789ae4654d
jayson7 <lexxiijoo70@gmail.com>
2021-12-22 00:45:41 +0100

    using smtp

--- using_smtp.py
+++ using_smtp.py
@@ -9,6 +9,4 @@ context = ssl.create_default_context()
 with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
     server.login("my@gmail.com", password)
     # TODO: Send email here
-    
-    
-# server.sendmail(sender_email, receiver_email, message) when you are ready! 😉 😜
+    # server.sendmail(sender_email, receiver_email, message) when you are ready! 😉 😜
